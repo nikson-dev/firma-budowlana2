9e73f4de537b3870a5a16661b622ec2b3a683733
nikson-dev <dmowskinikodem@gmail.com>
2026-02-26 18:25:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+// MENU MOBILE
+const toggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector("nav");
 
+toggle.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
+
+// LICZNIKI
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+    const update = () => {
+        const target = +counter.getAttribute("data-target");
+        const count = +counter.innerText;
+        const speed = target / 100;
+
+        if(count < target) {
+            counter.innerText = Math.ceil(count + speed);
+            setTimeout(update, 20);
+        } else {
+            counter.innerText = target;
+        }
+    };
+
+    update();
+});
